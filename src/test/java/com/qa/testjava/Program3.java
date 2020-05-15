0c4bf8619220e06612efdf81a256994c08cd81a9
Sonal <mittal.sonal04@gmail.com>
2020-05-15 02:58:36 -0400

    Signed-off-by: Sonal <mittal.sonal04@gmail.com>

---
+++ src/test/java/com/qa/testjava/Program3.java
@@ -0,0 +1,14 @@
+package com.qa.testjava;
+
+import org.testng.annotations.Test;
+
+public class Program3 {
+	
+	@Test
+	public void testprogram()
+	{
+		System.out.println("Hello world");
+	}
+	
+
+}
